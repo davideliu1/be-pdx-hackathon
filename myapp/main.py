0ce87ebd608310f307de2bec116c085e1ae2f0b5
Liu <david.e.liu@intel.com>
2020-07-11 17:20:48 -0700

    Now it is running

--- myapp/main.py
+++ myapp/main.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import boto3
+
+db = boto3.resource('dynamodb')
 
 app = Flask(__name__)
 
@@ -7,8 +10,15 @@ app = Flask(__name__)
 def hello():
     return "HELLO WE DEPLOYED"
 
+@app.route("/dbhit")
+def dbhit():
+    table = db.Table('Cued')
+    response = table.get_item(Key={'CueId': "oregon-medicaid-benefits", 'IssueId': "12341234"})
+    return response
+
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     app.debug = True
+    app.run()
